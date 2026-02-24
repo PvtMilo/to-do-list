df6a18d6f09de3d6360a2c74af6e6514be219cdc
PvtMilo <flip360.id.solo@gmail.com>
2026-02-24 20:34:44 +0700

    add crud endpoint and function

--- index.js
+++ index.js
@@ -4,6 +4,19 @@ import bodyParser from "body-parser";
 const app = express();
 const port = 3000;
 
+import pg from "pg";
+const { Client } = pg;
+
+const db = new Client({
+  user: "postgres",
+  password: "admin",
+  host: "localhost",
+  port: 5432,
+  database: "permalist",
+});
+
+await db.connect();
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
@@ -12,22 +25,78 @@ let items = [
   { id: 2, title: "Finish homework" },
 ];
 
-app.get("/", (req, res) => {
-  res.render("index.ejs", {
-    listTitle: "Today",
-    listItems: items,
-  });
+const readData = async () => {
+  const res = await db.query("SELECT * FROM todo");
+  return res.rows;
+};
+
+const addData = async (item) => {
+  const res = await db.query(
+    "INSERT INTO todo(title) VALUES($1) RETURNING * ",
+    [item],
+  );
+  return res.rows;
+};
+
+const modifyData = async (item, id) => {
+  const res = await db.query(
+    "UPDATE todo SET title = ($1) WHERE id = ($2) RETURNING *",
+    [item, id],
+  );
+  return res.rows;
+};
+
+const deleteData = async (id) => {
+  const res = await db.query("DELETE FROM todo WHERE id = ($1) RETURNING *", [
+    id,
+  ]);
+  return res.rows;
+};
+
+app.get("/", async (req, res) => {
+  try {
+    let items = await readData();
+    res.render("index.ejs", {
+      listTitle: "Today",
+      listItems: items,
+    });
+  } catch (error) {
+    console.log(error);
+  }
 });
 
-app.post("/add", (req, res) => {
+app.post("/add", async (req, res) => {
   const item = req.body.newItem;
-  items.push({ title: item });
-  res.redirect("/");
+  try {
+    const response = await addData(item);
+    items.push({ title: item });
+    res.redirect("/");
+  } catch (error) {
+    console.log(error);
+  }
 });
 
-app.post("/edit", (req, res) => {});
+app.post("/edit", async (req, res) => {
+  const { updatedItemId, updatedItemTitle } = req.body;
+  try {
+    const response = await modifyData(updatedItemTitle, updatedItemId);
+    res.redirect("/");
+  } catch (error) {
+    console.log(error);
+  }
+});
 
-app.post("/delete", (req, res) => {});
+app.post("/delete", async (req, res) => {
+  const { deleteItemId } = req.body;
+  console.log(deleteItemId)
+  try {
+    const response = await deleteData(deleteItemId);
+    console.log(response);
+    res.redirect("/");
+  } catch (error) {
+    console.log(error)
+  }
+});
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
